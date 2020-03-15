92b1c86046494736015867321a52c5b42c95e346
Alex Li <alex_li@asus.com>
2020-03-15 14:36:04 +0800

    Request camera permission by default

--- android/app/src/main/kotlin/com/eriko/audithelper/MainActivity.kt
+++ android/app/src/main/kotlin/com/eriko/audithelper/MainActivity.kt
@@ -1,18 +1,38 @@
 package com.eriko.audithelper
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
 
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
+import com.karumi.dexter.PermissionToken
+import com.karumi.dexter.listener.PermissionDeniedResponse
+import com.karumi.dexter.listener.PermissionGrantedResponse
+import com.karumi.dexter.listener.single.PermissionListener
+import android.Manifest.permission
+import com.karumi.dexter.Dexter
+import com.karumi.dexter.listener.PermissionRequest
 
 class MainActivity : AppCompatActivity() {
+    private var mCameraPermissionGranted = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
+
+        // Request camera permission
+        requestPermission()
+
+        capture_btn.setOnClickListener {
+            when (mCameraPermissionGranted) {
+                true -> Log.i(TAG, "Camera permission is granted")
+                else -> requestPermission()
+            }
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -29,5 +49,21 @@ class MainActivity : AppCompatActivity() {
             R.id.action_settings -> true
             else -> super.onOptionsItemSelected(item)
         }
+    }
+
+    private fun requestPermission() {
+        Dexter.withActivity(this)
+                .withPermission(permission.CAMERA)
+                .withListener(object : PermissionListener {
+                    override fun onPermissionGranted(response: PermissionGrantedResponse) {
+                        mCameraPermissionGranted = true
+                    }
+                    override fun onPermissionDenied(response: PermissionDeniedResponse) {
+                        mCameraPermissionGranted = false
+                    }
+                    override fun onPermissionRationaleShouldBeShown(permission: PermissionRequest, token: PermissionToken) {
+                        token.continuePermissionRequest()
+                    }
+                }).check()
     }
 }
