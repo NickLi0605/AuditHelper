9af4b280b5446349aacc1a2dcbdeb2b245ca1db3
Alex Li <alex_li@asus.com>
2020-03-23 21:39:47 +0800

    Modify TAG for logger to avoid wrong TAG of anonymous class

--- android/app/src/main/kotlin/com/eriko/audithelper/Extensions.kt
+++ android/app/src/main/kotlin/com/eriko/audithelper/Extensions.kt
@@ -5,8 +5,13 @@ import io.reactivex.disposables.Disposable
 
 val Any.TAG: String
     get() {
-        val tag = javaClass.simpleName
-        return if (tag.length < 23) tag else tag.substring(0, 23)
+        return if (!javaClass.isAnonymousClass) {
+            val name = javaClass.simpleName
+            if (name.length <= 23) name else name.substring(0, 23) // first 23 chars
+        } else {
+            val name = javaClass.name
+            if (name.length <= 23) name else name.substring(name.length - 23, name.length) // last 23 chars
+        }
     }
 
 fun Disposable.addTo(c: CompositeDisposable) {
